44d80a942c1fa0cb2344815dc7fe053e3aea12b6
Ivan Lazunin <code.wibbn@gmail.com>
2022-01-31 17:38:42 +0300

    update FCN model files structure

--- sberseg/models/FCN/__init__.py
+++ sberseg/models/FCN/__init__.py
@@ -4,26 +4,23 @@ import pytorch_lightning as pl
 from torch.nn import functional as F
 
 class FCN(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, num_classes, learning_rate=1e-3):
         super(FCN, self).__init__()
-        self.learning_rate = 1e-3
-        self.net = torchvision.models.segmentation.fcn_resnet50(
+        self.learning_rate = learning_rate
+        self.resnet50 = torchvision.models.segmentation.fcn_resnet50(
             pretrained = False, 
             progress = True, 
-            num_classes = 8
+            num_classes = num_classes
         )
-        # self.net = UNet(num_classes = 19, bilinear = False)
-        # self.net = torchvision.models.segmentation.deeplabv3_resnet50(pretrained = False, progress = True, num_classes = 19)
-        # self.net = ENet(num_classes = 19)
         
     def forward(self, x):
-        return self.net(x)
+        return self.resnet50(x)['out']
     
     def training_step(self, batch, batch_nb) :
         img, mask = batch
         img = img.float()
         mask = mask.long()
-        out = self.forward(img)['out']
+        out = self.forward(img)
         loss_val = F.cross_entropy(out, mask, ignore_index = 250)
         return {'loss' : loss_val}
     
@@ -34,7 +31,7 @@ class FCN(pl.LightningModule):
         img, mask = batch
         img = img.float()
         mask = mask.long()
-        out = self.forward(img)['out']
+        out = self.forward(img)
 
         loss_val = F.cross_entropy(out, mask, ignore_index = 250)
         return {'loss' : loss_val}
@@ -43,6 +40,15 @@ class FCN(pl.LightningModule):
         self.log("val/loss", outs['loss'])
     
     def configure_optimizers(self):
-        opt = torch.optim.Adam(self.net.parameters(), lr = self.learning_rate)
-        sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max = 10)
+        opt = torch.optim.Adam(self.parameters(), lr = self.learning_rate)
+        # sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max = 10)
+        sch = torch.optim.lr_scheduler.StepLR(opt, step_size=2)
         return [opt], [sch]
+
+if __name__ == '__main__':
+    in_batch, inchannel, in_h, in_w = 4, 3, 128, 128
+    x = torch.randn(in_batch, inchannel, in_h, in_w)
+    net = FCN(num_classes=8)
+    out = net(x)
+    print(out.shape)
+    print(out)
