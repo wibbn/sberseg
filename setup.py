1e95026c037ab1687157c3edf0ede3369851a3fe
Ivan Lazunin <code.wibbn@gmail.com>
2022-02-02 01:57:47 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setuptools.setup(
     name='sberseg',
     packages=setuptools.find_packages(),
     install_requires=[
-        'wandb',
-        'pytorch-lightning',
-        'numpy',
-        'opencv-python',
-        'python-box',
-        'click'
+        'wandb==0.12.10',
+        'pytorch-lightning==1.5.3',
+        'opencv-python==4.5.5.62',
+        'python-box==5.4.1',
+        'click==8.0.3'
     ]
 )
